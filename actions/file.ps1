456518e4a84eff107329559aa08e7bda100b67aa
Abhishek Brahmachary <abhishekb@nihilentanalytics.com>
2019-10-29 18:10:25 +0530

    first commit

--- actions/file.ps1
+++ actions/file.ps1
@@ -1,1 +1,8 @@
-sadsdsfdafile.txt
+$strComputer = $Host
+
+Clear
+$RAM = WmiObject Win32_ComputerSystem
+$MB = 1048576
+
+"Installed Memory: " + [int]($RAM.TotalPhysicalMemory /$MB) + " MB"
+#Create Session
