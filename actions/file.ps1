454911d12b8561c4086cc0fd30fa45805aefe1c0
Abhishek Brahmachary <abhishekb@nihilentanalytics.com>
2019-10-29 19:00:03 +0530

    first commit

--- actions/file.ps1
+++ actions/file.ps1
@@ -1,8 +1,24 @@
-$strComputer = $Host
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
 
-Clear
-$RAM = WmiObject Win32_ComputerSystem
-$MB = 1048576
+function Initialize-Assemblies {
+    $libDir = Join-Path $here "lib"
+    $assemblies = @{
+        "core" = Join-Path $libDir "netstandard1.3\YamlDotNet.dll";
+        "net45" = Join-Path $libDir "net45\YamlDotNet.dll";
+        "net35" = Join-Path $libDir "net35\YamlDotNet.dll";
+    }
 
-"Installed Memory: " + [int]($RAM.TotalPhysicalMemory /$MB) + " MB"
-#Create Session
+    try {
+        [YamlDotNet.Serialization.Serializer] | Out-Null
+    } catch [System.Management.Automation.RuntimeException] {
+        if ($PSVersionTable.PSEdition -eq "Core") {
+            return [Reflection.Assembly]::LoadFrom($assemblies["core"])
+        } elseif ($PSVersionTable.PSVersion.Major -ge 4) {
+            return [Reflection.Assembly]::LoadFrom($assemblies["net45"])
+        } else {
+            return [Reflection.Assembly]::LoadFrom($assemblies["net35"])
+        }
+    }
+}
+
+Initialize-Assemblies | Out-Null
