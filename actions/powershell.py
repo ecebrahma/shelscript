20c6a0aaed47b6746cb21a1f67bb96d26e60b5ed
Abhishek Brahmachary <abhishekb@nihilentanalytics.com>
2019-10-29 15:21:28 +0530

    first commit

--- actions/powershell.py
+++ actions/powershell.py
@@ -24,7 +24,7 @@ class MyAction(Action):
         # print(dir_path)
         for x in os.listdir(pwd):
             print x
-            if x == 'copy.ps1':
+            if x == 'Copy.ps1':
                 print("b is greater than a")
             # try:
             #     for y in os.listdir(x):
