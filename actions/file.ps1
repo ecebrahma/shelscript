96b95a5a54b58f77701b55aefade0840289e8480
Abhishek Brahmachary <abhishekb@nihilentanalytics.com>
2019-10-29 19:08:29 +0530

    first commit

--- actions/file.ps1
+++ actions/file.ps1
@@ -1,24 +1,44 @@
-$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
 
-function Initialize-Assemblies {
-    $libDir = Join-Path $here "lib"
-    $assemblies = @{
-        "core" = Join-Path $libDir "netstandard1.3\YamlDotNet.dll";
-        "net45" = Join-Path $libDir "net45\YamlDotNet.dll";
-        "net35" = Join-Path $libDir "net35\YamlDotNet.dll";
-    }
+$form = New-Object System.Windows.Forms.Form
+$form.Text = 'Data Entry Form'
+$form.Size = New-Object System.Drawing.Size(300,200)
+$form.StartPosition = 'CenterScreen'
 
-    try {
-        [YamlDotNet.Serialization.Serializer] | Out-Null
-    } catch [System.Management.Automation.RuntimeException] {
-        if ($PSVersionTable.PSEdition -eq "Core") {
-            return [Reflection.Assembly]::LoadFrom($assemblies["core"])
-        } elseif ($PSVersionTable.PSVersion.Major -ge 4) {
-            return [Reflection.Assembly]::LoadFrom($assemblies["net45"])
-        } else {
-            return [Reflection.Assembly]::LoadFrom($assemblies["net35"])
-        }
-    }
-}
+$OKButton = New-Object System.Windows.Forms.Button
+$OKButton.Location = New-Object System.Drawing.Point(75,120)
+$OKButton.Size = New-Object System.Drawing.Size(75,23)
+$OKButton.Text = 'OK'
+$OKButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
+$form.AcceptButton = $OKButton
+$form.Controls.Add($OKButton)
 
-Initialize-Assemblies | Out-Null
+$CancelButton = New-Object System.Windows.Forms.Button
+$CancelButton.Location = New-Object System.Drawing.Point(150,120)
+$CancelButton.Size = New-Object System.Drawing.Size(75,23)
+$CancelButton.Text = 'Cancel'
+$CancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
+$form.CancelButton = $CancelButton
+$form.Controls.Add($CancelButton)
+
+$label = New-Object System.Windows.Forms.Label
+$label.Location = New-Object System.Drawing.Point(10,20)
+$label.Size = New-Object System.Drawing.Size(280,20)
+$label.Text = 'Please enter the information in the space below:'
+$form.Controls.Add($label)
+
+$textBox = New-Object System.Windows.Forms.TextBox
+$textBox.Location = New-Object System.Drawing.Point(10,40)
+$textBox.Size = New-Object System.Drawing.Size(260,20)
+$form.Controls.Add($textBox)
+
+$form.Topmost = $true
+
+$form.Add_Shown({$textBox.Select()})
+$result = $form.ShowDialog()
+
+if ($result -eq [System.Windows.Forms.DialogResult]::OK)
+{
+    $x = $textBox.Text
+    $x}
