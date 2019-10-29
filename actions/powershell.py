a997efd9c078c9842fe124124ffc6744a3872aae
Abhishek Brahmachary <abhishekb@nihilentanalytics.com>
2019-10-29 16:28:13 +0530

    first commit

--- actions/powershell.py
+++ actions/powershell.py
@@ -17,30 +17,24 @@ class MyAction(Action):
         #newfile = dir_path.replace('.ps1', '.txt')
         print('--------------------------------------------')
         #print(newfile)
-        #file1=open(dir_path,"r") 
+        file1=open(dir_path,"r") 
         #file1=codecs.open(dir_path,'r')
         #file1 = codecs.open(dir_path, 'r',encoding=sys.getfilesystemencoding())
         #getdata = file1.read()
-        #getdata = file1.read()
+        getdata = file1.read()
         #getdata=getdata.decode('base64', 'strict') 
         #lines[line_num] = text
         #f.close()
         # file1 = open(dir_path,"r") 
         #getdata=file1.read()
         print('--------------------------------------------')
-        #print (getdata)
+        print (getdata)
         
         print ('/////////////////////////////////////')
-        #Open the file back and read the contents
-        f=open(dir_path, "r")
-        if f.mode == 'r': 
-            contents =f.read()
-            print contents
-        #or, readlines reads the individual line into a list
-        fl =f.readlines()
-        for x in fl:
-            print x
-        
+        # for root, dirs, files in os.walk("home"):
+        #     for file in files:
+        #         if file.endswith("."):
+        #             print(os.path.join(root, file))  
 
         print ('/////////////////////////////////////')
         
