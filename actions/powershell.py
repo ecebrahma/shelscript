6f9f5210c0183eae1af7478ece6f5976cf7f8272
Abhishek Brahmachary <abhishekb@nihilentanalytics.com>
2019-10-29 15:08:57 +0530

    first commit

--- actions/powershell.py
+++ actions/powershell.py
@@ -16,13 +16,13 @@ class MyAction(Action):
         print(dir_path)
         newfile = dir_path.replace('.ps1', '.txt')
         print('--------------------------------------------')
-        print(newfile)
-        file1 = open(dir_path,"r") 
-        getdata=file1.read()
-        print('--------------------------------------------')
-        print (getdata)
-        print(dir_path)
-        for x in os.listdir(pwd):
+        # print(newfile)
+        # file1 = open(dir_path,"r") 
+        # getdata=file1.read()
+        # print('--------------------------------------------')
+        # print (getdata)
+        # print(dir_path)
+        for x in os.listdir(__file__):
             print x
             # try:
             #     for y in os.listdir(x):
