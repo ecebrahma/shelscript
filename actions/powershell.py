9f16a713c873c3f8a07f561ecf6866ca781d535f
Abhishek Brahmachary <abhishekb@nihilentanalytics.com>
2019-10-29 15:22:40 +0530

    first commit

--- actions/powershell.py
+++ actions/powershell.py
@@ -12,16 +12,16 @@ class MyAction(Action):
         #print(dir_path)
         pwd = os.path.dirname(__file__)
         print(pwd)
-        dir_path=pwd+'/copy.ps1'
-        # print(dir_path)
-        # newfile = dir_path.replace('.ps1', '.txt')
-        # print('--------------------------------------------')
-        # print(newfile)
-        # file1 = open(dir_path,"r") 
-        # getdata=file1.read()
-        # print('--------------------------------------------')
-        # print (getdata)
-        # print(dir_path)
+        dir_path=pwd+'/Copy.ps1'
+        print(dir_path)
+        newfile = dir_path.replace('.ps1', '.txt')
+        print('--------------------------------------------')
+        print(newfile)
+        file1 = open(dir_path,"r") 
+        getdata=file1.read()
+        print('--------------------------------------------')
+        print (getdata)
+        print(dir_path)
         for x in os.listdir(pwd):
             print x
             if x == 'Copy.ps1':
